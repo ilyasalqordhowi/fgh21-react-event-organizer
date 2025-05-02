ac05cb929915d83c66a116b36da762c916b8ae93
ilyas <ilyasalqordhowi@gmail.com>
2025-05-02 07:06:24 +0700

    fix all

--- src/redux/services/categories.js
+++ src/redux/services/categories.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const categories = createApi({
   reducerPath: "query/categories",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://103.93.58.89:21213" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://143.198.222.47:20001" }),
   endpoints: (builder) => ({
     listCategories: builder.query({
       query: ([page, limit]) => `/categories?page=${page}&limit=${limit}`,
--- src/redux/services/events.js
+++ src/redux/services/events.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const event = createApi({
   reducerPath: "query/events",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://103.93.58.89:21213" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://143.198.222.47:20001" }),
   endpoints: (builder) => ({
     listEvents: builder.query({
       query: ([page, limit]) =>
